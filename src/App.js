7d69b1acd1b14dda6a88158d42afff950d1eada5
sibshahz <shahid5ssg@gmail.com>
2021-07-04 13:41:06 +0500

    added sign in handle submit method

--- src/App.js
+++ src/App.js
@@ -29,7 +29,6 @@ componentDidMount(){
             ...snapShot.data()
           }
         });
-        console.log(this.state);
       });
     }else{
       this.setState({currentUser: userAuth});
