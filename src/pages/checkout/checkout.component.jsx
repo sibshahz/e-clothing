9ad11eb64ae3ea0fd43afdff94892da9cf9d7cd5
Shahid Gillani <shahid5ssg@gmail.com>
2022-03-30 20:29:20 +0500

    added stripe payment integration into the store

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -5,6 +5,7 @@ import { createStructuredSelector } from 'reselect';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckoutPage = ({cartItems, total}) =>(
     <div className='checkout-page'>
@@ -32,6 +33,13 @@ const CheckoutPage = ({cartItems, total}) =>(
         }
         <div className='total'>
             <span>Total: ${total}</span>
+            <div className="test-warning">
+                *Please use the following test credit card for payments* 
+                <br />
+                    
+                    4242 4242 4242 4242 - Exp: 01/2023 - CVV: 123
+            </div>
+            <StripeCheckoutButton price={total} />
         </div>
     </div>
 );
