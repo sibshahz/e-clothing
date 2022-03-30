3eb681eb90498f7ec7d4dbe2c232aaf56051274f
Shahid Gillani <shahid5ssg@gmail.com>
2022-03-30 18:56:07 +0500

    moved our directory, and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {Route, Switch,Redirect} from 'react-router-dom';
 import './App.css';
-
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
@@ -52,7 +51,7 @@ componentWillUnmount(){
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
+          <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckoutPage} />
           <Route exact path='/signin' render={()=>this.props.currentUser? (<Redirect to='/' />): (<SignInAndSignUpPage />)}  />
           
