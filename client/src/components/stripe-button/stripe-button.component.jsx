97081d5805470e76d9805a0ab17eb23616bb271f
Shahid Gillani <shahid5ssg@gmail.com>
2022-04-17 20:45:28 +0500

    Seprated react app into client folder, added express server to help handle backend stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({price})=>{
     const priceForStripe=price*100;
     const publishableKey='pk_test_51Kj2JVKLXyPXuO2nLB1bVLlO5gvjt7t8oZjHiOMzZXaGyfJ2Iz8MQDFTsziBqI23cPc0thHeAPp0x4tMTMgGRqfP00c4lRkAyP';
 
     const onToken=token=>{
-        console.log(token);
-        alert("Payment Successful");
+        axios({
+            url:'payment',
+            method: 'post',
+            data:{
+                amount:priceForStripe,
+                token
+            }
+        }).then(response =>{
+            alert('Payment success');
+        }).catch(error =>{
+            console.log('Payment error: ', JSON.parse(error));
+            alert('There was an issue with your payment. Please make sure you use the provided credit cart.');
+        }) 
     };
     return(
         <StripeCheckout 
             label="Pay Now"
             name="E-Clothing Pvt Ltd."
-            billingAddress
-            shippingAddress
+            // billingAddress
+            // shippingAddress
             image="https://svgshare.com/i/CUz.svg"
             description={`Your total is $${price}`}
             amount={priceForStripe}
